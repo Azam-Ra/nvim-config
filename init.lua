a4e8d4ef42414c32e28603d4a562441231b7cb7c
Azam-Ra <azam.rahmatillaev@gmail.com>
2025-07-14 12:08:32 +0200

    Initialize the nvim-config repo

---
+++ init.lua
@@ -0,0 +1,10 @@
+require("miller.base")
+require("miller.remap")
+require("miller.autocmds")
+require("miller.lazy")
+require("miller.lsp")
+
+-- Plugins related config
+--   Colorscheme 
+vim.cmd.colorscheme("catppuccin-mocha")
+
