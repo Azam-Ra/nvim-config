2d2e2a5cfcfc85a06a2affdf6b9b3c3f2ec9d9d6
Azam Ra <azam.rahmatillaev@gmail.com>
2026-06-27 08:58:52 -0400

    maint: Changes relevant to tree-sitter breaking changes

--- lua/miller/plugins/treesitter.lua
+++ lua/miller/plugins/treesitter.lua
@@ -1,18 +1,19 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    lazy = false,
     build = ":TSUpdate",
     config = function()
-      require'nvim-treesitter.configs'.setup {
+      require'nvim-treesitter'.setup {
         -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+        ensure_installed = { "c", "cpp", "lua", "vim", "markdown", "python", "bash" },
 
         -- Install parsers synchronously (only applied to `ensure_installed`)
         -- sync_install = false,
 
         -- Automatically install missing parsers when entering buffer
         -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-        auto_install = false,
+        auto_install = true,
 
         -- List of parsers to ignore installing (or "all")
         -- ignore_install = { "javascript" },
